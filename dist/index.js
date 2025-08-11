062b79add0248ed5ba74fadaa42d6a2c42818434
Giau Lee <levangiau20032020@gmail.com>
2025-08-11 10:27:27 +0700

    update pack-4

--- dist/index.js
+++ dist/index.js
@@ -1,52 +1,9 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
-    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var __asyncValues = (this && this.__asyncValues) || function (o) {
-    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
-    var m = o[Symbol.asyncIterator], i;
-    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
-    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
-    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var http_1 = require("http");
-var url_1 = require("url");
-var ExpressPlus = /** @class */ (function () {
-    function ExpressPlus() {
+const http_1 = require("http");
+const url_1 = require("url");
+class ExpressPlus {
+    constructor() {
         this.routes = {
             GET: [],
             POST: [],
@@ -56,92 +13,53 @@ var ExpressPlus = /** @class */ (function () {
         };
         this.middlewares = [];
     }
-    ExpressPlus.prototype.addRoute = function (method, path, handler) {
-        this.routes[method].push({ path: path, handler: handler });
-    };
-    ExpressPlus.prototype.use = function (middleware) {
+    addRoute(method, path, handler) {
+        this.routes[method].push({ path, handler });
+    }
+    use(middleware) {
         this.middlewares.push(middleware);
-    };
-    ExpressPlus.prototype.get = function (path, handler) {
+    }
+    get(path, handler) {
         this.addRoute("GET", path, handler);
-    };
-    ExpressPlus.prototype.post = function (path, handler) {
+    }
+    post(path, handler) {
         this.addRoute("POST", path, handler);
-    };
-    ExpressPlus.prototype.put = function (path, handler) {
+    }
+    put(path, handler) {
         this.addRoute("PUT", path, handler);
-    };
-    ExpressPlus.prototype.patch = function (path, handler) {
+    }
+    patch(path, handler) {
         this.addRoute("PATCH", path, handler);
-    };
-    ExpressPlus.prototype.delete = function (path, handler) {
+    }
+    delete(path, handler) {
         this.addRoute("DELETE", path, handler);
-    };
-    ExpressPlus.prototype.listen = function (port, callback) {
-        var _this = this;
-        var server = (0, http_1.createServer)(function (req, res) {
-            var method = req.method;
-            var parsedUrl = (0, url_1.parse)(req.url || "", true);
-            var pathname = parsedUrl.pathname || "/";
-            var route = _this.routes[method].find(function (r) { return r.path === pathname; });
-            var extendedReq = req;
-            var extendedRes = res;
+    }
+    listen(port, callback) {
+        const server = (0, http_1.createServer)((req, res) => {
+            const method = req.method;
+            const parsedUrl = (0, url_1.parse)(req.url || "", true);
+            const pathname = parsedUrl.pathname || "/";
+            const route = this.routes[method].find((r) => r.path === pathname);
+            const extendedReq = req;
+            const extendedRes = res;
             extendedReq.query = parsedUrl.query;
-            extendedReq.getBody = function () { return __awaiter(_this, void 0, void 0, function () {
-                var chunks, _a, extendedReq_1, extendedReq_1_1, chunk, e_1_1, raw;
-                var _b, e_1, _c, _d;
-                return __generator(this, function (_e) {
-                    switch (_e.label) {
-                        case 0:
-                            if (extendedReq.body)
-                                return [2 /*return*/, extendedReq.body];
-                            chunks = [];
-                            _e.label = 1;
-                        case 1:
-                            _e.trys.push([1, 6, 7, 12]);
-                            _a = true, extendedReq_1 = __asyncValues(extendedReq);
-                            _e.label = 2;
-                        case 2: return [4 /*yield*/, extendedReq_1.next()];
-                        case 3:
-                            if (!(extendedReq_1_1 = _e.sent(), _b = extendedReq_1_1.done, !_b)) return [3 /*break*/, 5];
-                            _d = extendedReq_1_1.value;
-                            _a = false;
-                            chunk = _d;
-                            chunks.push(chunk);
-                            _e.label = 4;
-                        case 4:
-                            _a = true;
-                            return [3 /*break*/, 2];
-                        case 5: return [3 /*break*/, 12];
-                        case 6:
-                            e_1_1 = _e.sent();
-                            e_1 = { error: e_1_1 };
-                            return [3 /*break*/, 12];
-                        case 7:
-                            _e.trys.push([7, , 10, 11]);
-                            if (!(!_a && !_b && (_c = extendedReq_1.return))) return [3 /*break*/, 9];
-                            return [4 /*yield*/, _c.call(extendedReq_1)];
-                        case 8:
-                            _e.sent();
-                            _e.label = 9;
-                        case 9: return [3 /*break*/, 11];
-                        case 10:
-                            if (e_1) throw e_1.error;
-                            return [7 /*endfinally*/];
-                        case 11: return [7 /*endfinally*/];
-                        case 12:
-                            raw = Buffer.concat(chunks).toString();
-                            extendedReq.bodyRaw = raw;
-                            try {
-                                extendedReq.body = JSON.parse(raw);
-                            }
-                            catch (_f) {
-                                extendedReq.body = {};
-                            }
-                            return [2 /*return*/, extendedReq.body];
-                    }
-                });
-            }); };
+            extendedReq.getBody = async () => {
+                if (extendedReq.body)
+                    return extendedReq.body;
+                const chunks = [];
+                for await (const chunk of extendedReq) {
+                    chunks.push(chunk);
+                }
+                const raw = Buffer.concat(chunks).toString();
+                extendedReq.bodyRaw = raw;
+                try {
+                    extendedReq.body = JSON.parse(raw);
+                }
+                catch {
+                    extendedReq.body = {};
+                }
+                return extendedReq.body;
+            };
             extendedRes.status = function (code) {
                 extendedRes.statusCode = code;
                 return extendedRes;
@@ -150,9 +68,9 @@ var ExpressPlus = /** @class */ (function () {
                 extendedRes.setHeader("Content-Type", "application/json");
                 extendedRes.end(JSON.stringify(data));
             };
-            var i = 0;
-            var next = function () {
-                var middleware = _this.middlewares[i++];
+            let i = 0;
+            const next = () => {
+                const middleware = this.middlewares[i++];
                 if (middleware) {
                     middleware(extendedReq, extendedRes, next);
                 }
@@ -166,9 +84,6 @@ var ExpressPlus = /** @class */ (function () {
             next();
         });
         server.listen(port, callback);
-    };
-    return ExpressPlus;
-}());
-// ✅ Export cho cả ES Module và CommonJS
+    }
+}
 exports.default = ExpressPlus;
-module.exports = ExpressPlus;
--- dist/test.js
+++ dist/test.js
@@ -1,182 +1,106 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
-    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
-        }
-    return t;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var index_1 = require("./index");
-var utils_1 = require("@gieo/utils");
-var app = new index_1.default();
-app.post("/calc", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var _a, op, numbers, values, result;
-    return __generator(this, function (_b) {
-        switch (_b.label) {
-            case 0: return [4 /*yield*/, req.getBody()];
-            case 1:
-                _a = _b.sent(), op = _a.op, numbers = __rest(_a, ["op"]);
-                values = Object.values(numbers)
-                    .map(Number)
-                    .filter(function (v) { return !isNaN(v); });
-                if (values.length < 2) {
-                    return [2 /*return*/, res.status(400).json({
-                            statusCode: 400,
-                            success: false,
-                            message: "Need at least two numeric values",
-                        })];
-                }
-                try {
-                    switch (op) {
-                        case "sum":
-                            result = utils_1.sum.apply(void 0, values);
-                            break;
-                        case "sub":
-                            result = utils_1.sub.apply(void 0, values);
-                            break;
-                        case "mul":
-                            result = utils_1.mul.apply(void 0, values);
-                            break;
-                        case "div":
-                            result = utils_1.div.apply(void 0, values);
-                            break;
-                        default:
-                            return [2 /*return*/, res.status(400).json({
-                                    statusCode: 400,
-                                    success: false,
-                                    message: "Invalid operator",
-                                })];
-                    }
-                    return [2 /*return*/, res.status(200).json({
-                            statusCode: 200,
-                            success: true,
-                            result: result,
-                        })];
-                }
-                catch (err) {
-                    return [2 /*return*/, res.status(500).json({
-                            statusCode: 500,
-                            success: false,
-                            message: err.message,
-                        })];
-                }
-                return [2 /*return*/];
+const index_1 = __importDefault(require("./index"));
+const utils_1 = require("@gieo/utils");
+const app = new index_1.default();
+app.post("/calc", async (req, res) => {
+    const { op, ...numbers } = await req.getBody();
+    // Lấy tất cả giá trị số từ body
+    const values = Object.values(numbers)
+        .map(Number)
+        .filter((v) => !isNaN(v));
+    if (values.length < 2) {
+        return res.status(400).json({
+            statusCode: 400,
+            success: false,
+            message: "Need at least two numeric values",
+        });
+    }
+    let result;
+    try {
+        switch (op) {
+            case "sum":
+                result = (0, utils_1.sum)(...values);
+                break;
+            case "sub":
+                result = (0, utils_1.sub)(...values);
+                break;
+            case "mul":
+                result = (0, utils_1.mul)(...values);
+                break;
+            case "div":
+                result = (0, utils_1.div)(...values);
+                break;
+            default:
+                return res.status(400).json({
+                    statusCode: 400,
+                    success: false,
+                    message: "Invalid operator",
+                });
         }
-    });
-}); });
+        return res.status(200).json({
+            statusCode: 200,
+            success: true,
+            result,
+        });
+    }
+    catch (err) {
+        return res.status(500).json({
+            statusCode: 500,
+            success: false,
+            message: err.message,
+        });
+    }
+});
 // Các route cơ bản
-app.get("/", function (req, res) {
-    return res.status(200).json({ success: true, status: 200, result: "GET / OK" });
-});
-app.put("/", function (req, res) {
-    return res.status(200).json({ success: true, status: 200, result: "PUT / OK" });
-});
-app.post("/", function (req, res) {
-    return res.status(200).json({ success: true, status: 200, result: "POST / OK" });
-});
-app.patch("/", function (req, res) {
-    return res.status(200).json({ success: true, status: 200, result: "PATCH / OK" });
-});
-app.delete("/", function (req, res) {
-    return res.status(200).json({ success: true, status: 200, result: "DELETE / OK" });
-});
-var mathRoute = function (op) { return function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var _a, a, b, result;
-    return __generator(this, function (_b) {
-        switch (_b.label) {
-            case 0: return [4 /*yield*/, req.getBody()];
-            case 1:
-                _a = _b.sent(), a = _a.a, b = _a.b;
-                if (typeof a !== "number" || typeof b !== "number") {
-                    return [2 /*return*/, res.status(400).json({
-                            success: false,
-                            status: 400,
-                            result: "Invalid input. 'a' and 'b' must be numbers.",
-                        })];
-                }
-                try {
-                    result = op(a, b);
-                    res.status(200).json({ success: true, status: 200, result: result });
-                }
-                catch (err) {
-                    res
-                        .status(500)
-                        .json({ success: false, status: 500, result: err.message });
-                }
-                return [2 /*return*/];
-        }
-    });
-}); }; };
+app.get("/", (req, res) => res.status(200).json({ success: true, status: 200, result: "GET / OK" }));
+app.put("/", (req, res) => res.status(200).json({ success: true, status: 200, result: "PUT / OK" }));
+app.post("/", (req, res) => res.status(200).json({ success: true, status: 200, result: "POST / OK" }));
+app.patch("/", (req, res) => res.status(200).json({ success: true, status: 200, result: "PATCH / OK" }));
+app.delete("/", (req, res) => res.status(200).json({ success: true, status: 200, result: "DELETE / OK" }));
+const mathRoute = (op) => async (req, res) => {
+    const { a, b } = await req.getBody();
+    if (typeof a !== "number" || typeof b !== "number") {
+        return res.status(400).json({
+            success: false,
+            status: 400,
+            result: "Invalid input. 'a' and 'b' must be numbers.",
+        });
+    }
+    try {
+        const result = op(a, b);
+        res.status(200).json({ success: true, status: 200, result });
+    }
+    catch (err) {
+        res
+            .status(500)
+            .json({ success: false, status: 500, result: err.message });
+    }
+};
 // Math operations
 app.post("/sum", mathRoute(utils_1.sum));
 app.post("/sub", mathRoute(utils_1.sub));
 app.post("/mul", mathRoute(utils_1.mul));
-app.post("/div", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var _a, a, b, result;
-    return __generator(this, function (_b) {
-        switch (_b.label) {
-            case 0: return [4 /*yield*/, req.getBody()];
-            case 1:
-                _a = _b.sent(), a = _a.a, b = _a.b;
-                if (typeof a !== "number" || typeof b !== "number") {
-                    return [2 /*return*/, res.status(400).json({
-                            success: false,
-                            status: 400,
-                            result: "Invalid input. 'a' and 'b' must be numbers.",
-                        })];
-                }
-                if (b === 0) {
-                    return [2 /*return*/, res
-                            .status(400)
-                            .json({ success: false, status: 400, result: "Cannot divide by zero" })];
-                }
-                result = (0, utils_1.div)(a, b);
-                res.status(200).json({ success: true, status: 200, result: result });
-                return [2 /*return*/];
-        }
-    });
-}); });
-app.listen(3000, function () {
+app.post("/div", async (req, res) => {
+    const { a, b } = await req.getBody();
+    if (typeof a !== "number" || typeof b !== "number") {
+        return res.status(400).json({
+            success: false,
+            status: 400,
+            result: "Invalid input. 'a' and 'b' must be numbers.",
+        });
+    }
+    if (b === 0) {
+        return res
+            .status(400)
+            .json({ success: false, status: 400, result: "Cannot divide by zero" });
+    }
+    const result = (0, utils_1.div)(a, b);
+    res.status(200).json({ success: true, status: 200, result });
+});
+app.listen(3000, () => {
     console.log("✅ Server is running at http://localhost:3000");
 });
